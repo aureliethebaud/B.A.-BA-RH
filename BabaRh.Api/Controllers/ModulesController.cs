1e7ac5bf12bcb4f7c79367ed512169a61593b3e0
Aurelie <aurelie.thebaud@hotmail.fr>
2021-06-13 17:07:11 +0200

    maj_edit

--- BabaRh.Api/Controllers/ModulesController.cs
+++ BabaRh.Api/Controllers/ModulesController.cs
@@ -20,14 +20,21 @@ namespace BabaRh.Api.Controllers
             return this.Ok(result);
         }
 
-        // GET api/modules/id
+
+        // GET api/modules/moduleLib
         [HttpGet]
         public IHttpActionResult Get(string moduleLib)
         {
-            var result = moduleAccessLayer.Get(moduleLib);
-
+            var fromDb = moduleAccessLayer.Get(moduleLib);
+            if (fromDb == null)
+                return this.NotFound();
+            var result = new Module
+            {
+                ModuleLib = fromDb.ModuleLib
+            };
             return this.Ok(result);
         }
+                        
 
         [HttpPost]
         public IHttpActionResult Create([FromBody] Module module)
@@ -41,6 +48,21 @@ namespace BabaRh.Api.Controllers
             
             moduleAccessLayer.Add(moduleToAdd);
             return this.Ok("created");
+        }
+
+
+    
+        [HttpPut]
+        public IHttpActionResult Update(string moduleLib, [FromBody] Module module)
+        {
+            var moduleToUpdate = new AccessLayer.Models.Module
+            {
+                ModuleLib = module.ModuleLib
+            };
+
+            moduleAccessLayer.Update(moduleToUpdate);
+
+            return this.Ok("updated");
         }
 
     }
